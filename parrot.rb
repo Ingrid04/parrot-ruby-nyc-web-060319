40b301bdd4c590df8b1dc340b173c1396e2f8dc8
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-23 22:43:54 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,2 @@
-def parrot(kind=      #Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot (kind="Squawk!")      #Create method `parrot` that     puts                                 outputs a given phrase and
+                                 # returns the phrase
